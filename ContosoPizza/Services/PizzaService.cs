74bd882157605dc807711eb40cc0f456a6df9f05
Marco Salazar <122067295+Marco-str@users.noreply.github.com>
2023-05-26 13:31:38 -0300

    Configuracion de API web

--- ContosoPizza/Services/PizzaService.cs
+++ ContosoPizza/Services/PizzaService.cs
@@ -1,46 +1,96 @@
-﻿using ContosoPizza.Data;
+﻿// using ContosoPizza.Data;
+// using ContosoPizza.Models;
+
+// namespace ContosoPizza.Services
+// {
+//     public class PizzaService
+//     {
+        
+//         private readonly PizzaContext _context = default!;
+
+//         public PizzaService(PizzaContext context) 
+//         {
+//             _context = context;
+//         }
+        
+//         public IList<Pizza> GetPizzas()
+//         {
+//             if(_context.Pizzas != null)
+//             {
+//                 return _context.Pizzas.ToList();
+//             }
+//             return new List<Pizza>();
+//         }
+
+//         public void AddPizza(Pizza pizza)
+//         {
+//             if (_context.Pizzas != null)
+//             {
+//                 _context.Pizzas.Add(pizza);
+//                 _context.SaveChanges();
+//             }
+//         }
+
+//         public void DeletePizza(int id)
+//         {
+//             if (_context.Pizzas != null)
+//             {
+//                 var pizza = _context.Pizzas.Find(id);
+//                 if (pizza != null)
+//                 {
+//                     _context.Pizzas.Remove(pizza);
+//                     _context.SaveChanges();
+//                 }
+//             }            
+//         } 
+//     }
+// }
+
 using ContosoPizza.Models;
 
-namespace ContosoPizza.Services
+namespace ContosoPizza.Services;
+
+public static class PizzaService 
 {
-    public class PizzaService
+    static List<Pizza> Pizzas { get; }
+    static int nextId = 3;
+    static PizzaService()
     {
-        private readonly PizzaContext _context = default!;
-
-        public PizzaService(PizzaContext context) 
+        Pizzas = new List<Pizza>
         {
-            _context = context;
-        }
-        
-        public IList<Pizza> GetPizzas()
-        {
-            if(_context.Pizzas != null)
-            {
-                return _context.Pizzas.ToList();
-            }
-            return new List<Pizza>();
-        }
-
-        public void AddPizza(Pizza pizza)
-        {
-            if (_context.Pizzas != null)
-            {
-                _context.Pizzas.Add(pizza);
-                _context.SaveChanges();
-            }
-        }
-
-        public void DeletePizza(int id)
-        {
-            if (_context.Pizzas != null)
-            {
-                var pizza = _context.Pizzas.Find(id);
-                if (pizza != null)
-                {
-                    _context.Pizzas.Remove(pizza);
-                    _context.SaveChanges();
-                }
-            }            
-        } 
+            new Pizza { Id = 1, Name = "Margherita", IsGlutenFree = false },
+            new Pizza { Id = 2, Name = "Pepperoni", IsGlutenFree = false },
+            new Pizza { Id = 3, Name = "Hawaiian", IsGlutenFree = false },
+            new Pizza { Id = 4, Name = "Veggie", IsGlutenFree = true },
+        };
     }
+
+public static List<Pizza> GetAll() => Pizzas;
+
+public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+public static void Add(Pizza pizza)
+{
+    pizza.Id = nextId++;
+    Pizzas.Add(pizza);
 }
+
+public static void Delete(int id)
+{
+    var pizza = Get(id);
+    if (pizza is null)
+        return;
+
+    Pizzas.Remove(pizza);
+}
+public static void Update(Pizza pizza)
+{
+    var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+    if (index == -1)
+        return;
+
+    Pizzas[index] = pizza;
+}
+}
+
+
